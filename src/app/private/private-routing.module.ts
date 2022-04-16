6cc7788e1372f835aad5e90890a451d546933e26
saumyadipisteer <saumyadip.biswas@gmail.com>
2022-04-16 21:32:34 +0530

    #7 - routes for bug report component updated

--- src/app/private/private-routing.module.ts
+++ src/app/private/private-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateBugReportComponent } from './create-bug-report/create-bug-report.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '',
+    pathMatch: 'full',
+  },
+  {
+    path: 'details',
+    component: CreateBugReportComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class PrivateRoutingModule { }
+export class PrivateRoutingModule {}
