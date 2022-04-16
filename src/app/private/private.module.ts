5b4837d42d77f372ca587c73c26c950e91cb33f3
saumyadipisteer <saumyadip.biswas@gmail.com>
2022-04-16 21:37:53 +0530

    #10 - clean up

--- src/app/private/private.module.ts
+++ src/app/private/private.module.ts
@@ -3,10 +3,6 @@ import { CommonModule } from '@angular/common';
 
 import { PrivateRoutingModule } from './private-routing.module';
 import { PrivateComponent } from './private.component';
-import { IngredientComponent } from './ingredient/ingredient.component';
-import { IngredientListComponent } from './ingredient/ingredient-list/ingredient-list.component';
-import { IngredientFormComponent } from './ingredient/ingredient-form/ingredient-form.component';
-import { RecipeMessageComponent } from './recipe-message/recipe-message.component';
 import { CreateBugReportComponent } from './create-bug-report/create-bug-report.component';
 import { ReportDetailsComponent } from './create-bug-report/report-details/report-details.component';
 
@@ -16,10 +12,7 @@ import { ReportDetailsComponent } from './create-bug-report/report-details/repor
 @NgModule({
   declarations: [
     PrivateComponent,
-    IngredientComponent,
-    IngredientListComponent,
-    IngredientFormComponent,
-    RecipeMessageComponent,
+    
     CreateBugReportComponent,
     ReportDetailsComponent,
   ],
