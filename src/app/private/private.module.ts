a47b6a8410c2118f1d09f7196a23c1b9bc86aff9
saumyadipisteer <saumyadip.biswas@gmail.com>
2022-04-16 23:02:59 +0530

    #16 - primeng modules imported

--- src/app/private/private.module.ts
+++ src/app/private/private.module.ts
@@ -5,17 +5,20 @@ import { PrivateRoutingModule } from './private-routing.module';
 import { PrivateComponent } from './private.component';
 import { CreateBugReportComponent } from './create-bug-report/create-bug-report.component';
 import { ReportDetailsComponent } from './create-bug-report/report-details/report-details.component';
+import { StepsModule } from 'primeng/steps';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputNumberModule } from 'primeng/inputnumber';
 
-
-
+const primeng = [StepsModule, ButtonModule, InputTextModule, InputNumberModule];
 
 @NgModule({
   declarations: [
     PrivateComponent,
-    
+
     CreateBugReportComponent,
     ReportDetailsComponent,
   ],
-  imports: [CommonModule, PrivateRoutingModule],
+  imports: [CommonModule, PrivateRoutingModule, ...primeng],
 })
 export class PrivateModule {}
