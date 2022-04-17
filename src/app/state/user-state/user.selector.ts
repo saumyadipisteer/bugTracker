fd3d663f2ae12e8d0f31f0952567d837f3d3f0ad
saumyadipisteer <saumyadip.biswas@gmail.com>
2022-04-17 16:43:31 +0530

    #61 - new selector added

--- src/app/state/user-state/user.selector.ts
+++ src/app/state/user-state/user.selector.ts
@@ -1,14 +1,16 @@
-import { createFeatureSelector, createSelector } from "@ngrx/store";
-import { LoginInfo } from "src/app/interface/user-store.interface";
+import { createFeatureSelector, createSelector } from '@ngrx/store';
+import { LoginInfo } from 'src/app/interface/user-store.interface';
 
-export const selectLoginStatusState = createFeatureSelector<LoginInfo>("login")
+export const selectLoginStatusState = createFeatureSelector<LoginInfo>('login');
 
 export const isLoggedin = createSelector(
-    selectLoginStatusState,
-    (login)=> login.status
-)
+  selectLoginStatusState,
+  (login) => login.status
+);
 
-export const isLoggedOut = createSelector(
-    isLoggedin,
-    (login)=> !login
-)
+export const currentUser = createSelector(
+  selectLoginStatusState,
+  (login) => login
+);
+
+export const isLoggedOut = createSelector(isLoggedin, (login) => !login);
