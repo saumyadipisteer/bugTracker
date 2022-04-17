c299a4decd95cadd19929fcde6412fc618209b24
saumyadipisteer <saumyadip.biswas@gmail.com>
2022-04-17 10:17:49 +0530

    #30 - data to dumb component

--- src/app/private/create-bug-report/report-details/report-details.component.ts
+++ src/app/private/create-bug-report/report-details/report-details.component.ts
@@ -7,6 +7,7 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class ReportDetailsComponent implements OnInit {
   @Input() severityOptions: string[];
+  @Input() statusOptions: string[];
   constructor() {}
 
   ngOnInit(): void {}
