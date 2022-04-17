d2d74b8ba29d9ca1daeb37fc62d516dda41711ab
saumyadipisteer <saumyadip.biswas@gmail.com>
2022-04-17 10:58:36 +0530

    #41 - function to create a form

--- src/app/private/create-bug-report/report-details/report-details.component.ts
+++ src/app/private/create-bug-report/report-details/report-details.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Fields } from '../../interface/common';
 
 @Component({
@@ -11,15 +11,30 @@ export class ReportDetailsComponent implements OnInit {
   @Input() severityOptions: string[];
   @Input() statusOptions: string[];
   @Input() fields: Fields;
-  
+
   fg: FormGroup;
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.fg = this.createForm();
+  }
 
-  private createForm(): FormGroup{
+  /**
+   * @description Creates a form with null value and required validators by default depending upon
+   * the data
+   * @param none
+   * @returns `FormGroup`
+   */
+  private createForm(): FormGroup {
     const control = {};
-    
+    Object.keys(this.fields).forEach((field) => {
+      control[field] = [
+        { value: null, disabled: this.fields[field]?.disabled },
+      ];
+      this.fields[field]?.required
+        ? Validators.required
+        : Validators.nullValidator;
+    });
     return new FormBuilder().group(control);
   }
 }
