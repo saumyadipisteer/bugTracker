50b59529299ccc020714694698690f05a354b14a
saumyadipisteer <saumyadip.biswas@gmail.com>
2022-04-17 10:02:14 +0530

    #26 - data to dumb component

--- src/app/private/create-bug-report/create-bug-report.component.ts
+++ src/app/private/create-bug-report/create-bug-report.component.ts
@@ -3,13 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-create-bug-report',
   templateUrl: './create-bug-report.component.html',
-  styleUrls: ['./create-bug-report.component.scss']
+  styleUrls: ['./create-bug-report.component.scss'],
 })
 export class CreateBugReportComponent implements OnInit {
+  severityOptions: string[] = ['--Select--', 'Low', 'Medium', 'High']; //- TODO: must come from an API
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
