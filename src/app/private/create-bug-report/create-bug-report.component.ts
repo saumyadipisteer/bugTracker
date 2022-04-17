8b713ceb4447526b1bd8e626d711789c59245dec
saumyadipisteer <saumyadip.biswas@gmail.com>
2022-04-17 10:43:19 +0530

    #37 - field information added

--- src/app/private/create-bug-report/create-bug-report.component.ts
+++ src/app/private/create-bug-report/create-bug-report.component.ts
@@ -6,6 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-bug-report.component.scss'],
 })
 export class CreateBugReportComponent implements OnInit {
+  field = {
+    subject: {
+      label: 'Subject',
+      field: 'subject',
+      required: true,
+      disabled: false,
+    },
+    severity: {
+      label: 'Severity',
+      field: 'severity',
+      required: true,
+      disabled: false,
+    },
+    status: {
+      label: 'Status',
+      field: 'severity',
+      required: true,
+      disabled: false,
+    },
+    describeTheBug: {
+      label: 'Describe the bug',
+      field: 'describeTheBug',
+      required: false,
+      disabled: false,
+    },
+  };
   severityOptions: string[] = ['--Select--', 'Low', 'Medium', 'High']; //- TODO: must come from an API
   statusOptions: string[] = ['--Select--', 'Open', 'TBD']; //- TODO: More option for depending on role
   constructor() {}
